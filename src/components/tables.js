6c59e81920a66f37259a0efa9239f0116cb1dcb8
mihay42 <mihay42@gmail.com>
2024-01-20 19:22:52 -0800

    Prototype 22 - tables

--- src/components/tables.js
+++ src/components/tables.js
@@ -8,10 +8,21 @@ const { TABLE_CELL_DIV, TABLE_HEADER_SEP, SECTION_LINE_BREAK } = require('../uti
  * @param {Array[]} cells - array of cells
  * @returns {string} table row
  */
-const tableRows = (cells) => {
-    const myCells = cells.join(TABLE_CELL_DIV)
-    const row = surround(TABLE_CELL_DIV, myCells)
-    return row + SECTION_LINE_BREAK
+const tableRows = (rows) => {
+    let myRows = []
+    for (const row of rows) {
+        if (row.length !== rows[0].length) {
+            throw new Error('Table rows must all be the same length')
+        }
+        // Join the cells with the table cell divider
+        let myRow = row.join(TABLE_CELL_DIV)
+        // Surround the row with the table cell divider
+        myRow = surround(TABLE_CELL_DIV, row)
+        // Add the row to the rows array
+        myRows.push(myRow)
+    }
+    const markdownRows = myRows.join(SECTION_LINE_BREAK)
+    return markdownRows + SECTION_LINE_BREAK
 }
 
 /**
